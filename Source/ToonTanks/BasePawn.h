9e9dcda259011af7ac9a42c083c1148a21d94119
Yunus Emre Akkaş <58690489+yunusemreakkas16@users.noreply.github.com>
2024-01-26 19:12:22 +0300

    Capsule Collider added.

--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -26,4 +26,8 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	UPROPERTY()
+	class UCapsuleComponent* CapsuleComp;
+
 };
