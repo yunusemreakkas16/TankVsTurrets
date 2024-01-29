014c53a41bd98018e9198dae70ac675517452693
Yunus Emre Akkaş <58690489+yunusemreakkas16@users.noreply.github.com>
2024-01-29 18:13:12 +0300

    TankChildClass added.

---
+++ Source/ToonTanks/Tank.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tank.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+ATank::ATank()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+    SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("ArmComponent"));
+    SpringArmComponent ->SetupAttachment(RootComponent);
+
+    CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+    CameraComponent ->SetupAttachment(SpringArmComponent);
+}
+
+/*
+// Called when the game starts or when spawned
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ATank::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+*/
+
+
---
+++ Source/ToonTanks/Tank.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tank.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATank : public ABasePawn
+{	
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ATank();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Components")
+	class USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(EditAnywhere, Category = "Components")
+	class UCameraComponent* CameraComponent;
+
+
+
+	
+};
