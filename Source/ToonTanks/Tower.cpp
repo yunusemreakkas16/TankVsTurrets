6d357a80dc9ca24cbcc6aee46abd601d928b6588
Yunus Emre Akkaş <58690489+yunusemreakkas16@users.noreply.github.com>
2024-02-03 20:41:49 +0300

    Tower rotation mechanic added.

---
+++ Source/ToonTanks/Tower.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "Tower.h"
+#include "Tank.h"
+#include "Kismet/GameplayStatics.h"
+
+
+ATower::ATower()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ATower::BeginPlay()
+{
+    Super::BeginPlay();
+
+    Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
+
+void ATower::Tick(float DeltaTime)
+{
+    if(Tank)
+    {
+        float Distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
+        if(Distance<=FireRange)
+        {
+            RotateTurret(Tank->GetActorLocation());   
+        }
+    }
+}
+
+
---
+++ Source/ToonTanks/Tower.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATower : public ABasePawn
+{
+	GENERATED_BODY()
+
+public:
+
+	ATower();
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+
+	class ATank* Tank;
+
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	float FireRange = 300.f;
+};
